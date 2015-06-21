baa12a5a5f1b0cd24938fefbab7e9c29eaf2d763
steven harradine <steven.harradine@telus.com>
2015-06-21 11:49:03 -0400

    remove cd to directory, remove single space that started each line

--- updater/compile.sh
+++ updater/compile.sh
@@ -1,3 +1,2 @@
- #!/bin/bash
- cd /etc/sarah/www/apps/SARAH-reader/updater
- javac -cp "../../../libs/mysql-connector-java-5.1.32-bin.jar:../../../libs/dom4j-1.6.1.jar:../../../libs/nekohtml.jar:../../../libs/json-simple-1.1.1.jar:../../../libs/feed4j.jar:../../../libs/xercesImpl-2.7.1.jar:." ReaderUpdater.java
+#!/bin/bash
+javac -cp "../../../libs/mysql-connector-java-5.1.32-bin.jar:../../../libs/dom4j-1.6.1.jar:../../../libs/nekohtml.jar:../../../libs/json-simple-1.1.1.jar:../../../libs/feed4j.jar:../../../libs/xercesImpl-2.7.1.jar:." ReaderUpdater.java
